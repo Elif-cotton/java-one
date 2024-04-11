1ccd22f366870f267747f8b81c69682809bcb431
“elif-cotton” <elif.watercolors@gmail.com>
2024-04-11 19:27:58 -0400

    usando métodos

---
+++ java/tercero-screenmatch/src/Principal.java
@@ -0,0 +1,31 @@
+public class Principal {
+    public static void main(String[] args) {
+
+        Pelicula miPelicula = new Pelicula();
+
+        miPelicula.nombre ="Encanto";
+        miPelicula.fechaDeLanzamiento = 2021;
+        miPelicula.duracionEnMinutos = 120;
+
+        miPelicula.muestraFichaTecnica();
+        miPelicula.evalua(10);
+        miPelicula.evalua(10);
+        System.out.println(miPelicula.sumaDeLasEvaluaciones);
+        System.out.println(miPelicula.totalDeLasEvaluaciones);
+        System.out.println(miPelicula.calculaMedia());
+
+        Pelicula otraPelicula = new Pelicula();
+        otraPelicula.nombre = "Matrix";
+        otraPelicula.fechaDeLanzamiento = 1998;
+        otraPelicula.duracionEnMinutos = 180;
+
+        otraPelicula.muestraFichaTecnica();
+
+        otraPelicula.evalua(10);
+        otraPelicula.evalua(10);
+        otraPelicula.evalua(7.8);
+        System.out.println("Media de evaluaciones de la película: " +otraPelicula.calculaMedia());
+
+    }
+
+}
