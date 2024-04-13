699de7153a609f4b020bd093d9703920ab9e9851
“elif-cotton” <elif.watercolors@gmail.com>
2024-04-13 13:34:53 -0400

    agregar interface

--- java/tercero-screenmatch/src/Principal.java
+++ java/tercero-screenmatch/src/Principal.java
@@ -1,4 +1,6 @@
+import com.aluracursos.screenmatch.calculos.CalculadoraDeTiempo;
 import com.aluracursos.screenmatch.modelos.Pelicula;
+import com.aluracursos.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -15,7 +17,29 @@ public class Principal {
         miPelicula.evalua(10);
         miPelicula.evalua(7.8);
         System.out.println("Media de evaluaciones de la película: " + miPelicula.calculaMedia());
-        
+
+        Serie casaDragon = new Serie();
+        casaDragon.setNombre("La casa del dragón");
+        casaDragon.setFechaDeLanzamiento(2021);
+        casaDragon.setTemporadas(1);
+        casaDragon.setMinutosPorEpisodios(50);
+        casaDragon.setEpisodiosPorTemporada(10);
+        casaDragon.muestraFichaTecnica();
+        System.out.println(casaDragon.getDuracionEnMinutos());
+
+        Pelicula otraPelicula = new Pelicula();
+        otraPelicula.setNombre("Matrix");
+        otraPelicula.setFechaDeLanzamiento(1998);
+        otraPelicula.setDuracionEnMinutos(180);
+        otraPelicula.setIncluidoEnElPlan(true);
+
+        CalculadoraDeTiempo calculadora = new CalculadoraDeTiempo();
+        calculadora.incluye(miPelicula);
+        calculadora.incluye(casaDragon);
+        calculadora.incluye(otraPelicula);
+        System.out.println("Tiempo necesario para ver tus películas favoritas estas vacaciones: " + calculadora.getTiempoTotal() +" minutos");
+
+
     }
 
 }
