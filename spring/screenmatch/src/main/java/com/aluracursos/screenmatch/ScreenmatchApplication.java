33786fd25f3b4ae0079dbac2caa739d81753f524
“elif-cotton” <elif.watercolors@gmail.com>
2024-04-27 18:57:06 -0400

    mappear y genéricos

--- spring/screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ spring/screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,8 @@
 package com.aluracursos.screenmatch;
 
+import com.aluracursos.screenmatch.model.DatosSerie;
+import com.aluracursos.screenmatch.service.ConsumoAPI;
+import com.aluracursos.screenmatch.service.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +16,16 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hola mundo desde Spring");
+		System.out.println("Primer proyecto Spring sin Web");
+		var consumoApi = new ConsumoAPI();
+		//var json = consumoApi.obtenerDatos("https://coffee.alexflipnote.dev/random.json");
+		//var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season=1&apikey=727776e2");
+		var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&apikey=727776e2");
+
+		System.out.println(json);
+
+		ConvierteDatos conversor = new ConvierteDatos();
+		DatosSerie datos = conversor.obtenerDatos(json, DatosSerie.class);
+		System.out.println(datos);
 	}
 }
