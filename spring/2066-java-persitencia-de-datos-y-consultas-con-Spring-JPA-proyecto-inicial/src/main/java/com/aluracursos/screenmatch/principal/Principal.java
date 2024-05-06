8024715377ebe9896141a536edd5fc667bd2fee0
“elif-cotton” <elif.watercolors@gmail.com>
2024-05-06 18:47:04 -0400

    Datos sensibles seguros

--- spring/2066-java-persitencia-de-datos-y-consultas-con-Spring-JPA-proyecto-inicial/src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ spring/2066-java-persitencia-de-datos-y-consultas-con-Spring-JPA-proyecto-inicial/src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -90,10 +90,7 @@ public class Principal {
     }
 
     private void mostrarSeriesBuscadas() {
-        List<Serie> series = new ArrayList<>();
-        series = datosSeries.stream()
-                .map(d -> new Serie(d))
-                .collect(Collectors.toList());
+        List<Serie> series = repositorio.findAll();
 
         series.stream()
                 .sorted(Comparator.comparing(Serie::getGenero))
